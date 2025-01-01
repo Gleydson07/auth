1efa2e40bbaa86af01845e75bec1af9e63f2f6bd
Gleydson Santos <gassantos.dev@gmail.com>
2025-01-01 12:26:27 -0300

    remove ClientsModule from RabbitMQModule

--- src/rabbitmq/rabbitmq.module.ts
+++ src/rabbitmq/rabbitmq.module.ts
@@ -1,36 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { ConfigService  } from "@nestjs/config";
 import { RabbitmqService } from "./rabbitmq.service";
 
 @Module({
-  imports: [
-    ClientsModule.registerAsync([{
-      name: "RabbitMQ",
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        name: configService.get<string>('APP_NAME'),
-        transport: Transport.RMQ,
-        options: {
-          urls: [configService.get<string>('RABBITMQ_URL')],
-          queueOptions: {
-            arguments: {
-              'x-message-ttl': +configService.get<number>('RABBITMQ_TTL_MSGS'), // TTL de mensagens(60 segundos)
-              'x-dead-letter-exchange': configService.get<string>('RABBITMQ_EXCHANGE_EXPIRED_MSGS'), // Exchange para mensagens expiradas
-            }
-          },
-          exchange: {
-            name: configService.get<string>('RABBITMQ_EXCHANGE_NAME'),
-            type: configService.get<string>('RABBITMQ_EXCHANGE_TYPE'), // 'fanout', 'direct', 'topic', 'headers'
-            echangeOpts: {
-              durable: true
-            }
-          },
-        }
-      })
-    }]),
-  ],
   providers: [RabbitmqService],
-  exports: [RabbitmqService, ClientsModule]
+  exports: [RabbitmqService]
 })
 export class RabbitmqModule {}
