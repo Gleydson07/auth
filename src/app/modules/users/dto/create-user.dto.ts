7683926e8840112ec3d01fdca8defa123b70ff38
Gleydson Santos <gassantos.dev@gmail.com>
2025-02-10 21:32:06 -0300

    fix error from graphql request on the dto format

--- src/app/modules/users/dto/create-user.dto.ts
+++ src/app/modules/users/dto/create-user.dto.ts
@@ -1,19 +1,25 @@
+import { Field, InputType } from '@nestjs/graphql';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
+@InputType()
 export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
+  @Field()
   name: string;
 
   @IsString()
   @IsNotEmpty()
+  @Field()
   lastname: string;
 
   @IsEmail()
   @IsNotEmpty()
+  @Field()
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @Field()
   password: string;
 }
