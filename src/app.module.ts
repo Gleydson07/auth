e0667088cced51cc927fa851cd99c2b1c8141cc9
Gleydson Santos <gassantos.dev@gmail.com>
2025-04-13 01:13:20 -0300

    active rabbitmq route

--- src/app.module.ts
+++ src/app.module.ts
@@ -87,7 +87,7 @@ export const prefix = 'ms-auth/api/v1';
           { path: `/:id/addresses`, module: AddressesModule },
         ],
       },
-      // { path: `${prefix}/system/rabbitmq`, module: RabbitmqModule },
+      { path: `${prefix}/system/rabbitmq`, module: RabbitmqModule },
     ]),
   ],
   providers: [
